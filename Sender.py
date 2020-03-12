0635da47930a56e9f1c0d175e0e775a4d9c05b6f
Prashanth <prashanthbalaji96@gmail.com>
2020-03-12 15:35:57 -0600

    Try except in sender

--- Sender.py
+++ Sender.py
@@ -2,18 +2,22 @@ import socket
 import pickle
 
 def sendToServer(data):
-    if type(data) != type({}):
-        return -2 #incorrect data type
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("localhost", 8000))
-    client.sendall(pickle.dumps(data))
-    incoming = client.recv(4096)
-    response = pickle.loads(incoming)
-    if response["status"] == 1:
+    try:
+        if type(data) != type({}):
+            return -2 #incorrect data type
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(("localhost", 8000))
+        client.sendall(pickle.dumps(data))
+        incoming = client.recv(4096)
+        response = pickle.loads(incoming)
+        if response["status"] == 1:
+            client.close()
+            return 1
+        else:
+            return -1
+    except:
         client.close()
-        return 1
-    else:
-        return -1
+
 
 
 
